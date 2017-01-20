515aa4fcd760de296bd6f914d61464b902514644
LucyCo <lucy.co1608@gmail.com>
2017-01-21 01:52:31 +0200

    fixed data members

--- factory.py
+++ factory.py
@@ -5,10 +5,9 @@ CREATURES_LIST_FILE = "creatures.json"
 
 
 class Creature:
-    __creature_name = ""
 
     def __init__(self, creature_name):
-        self.__creature_name = creature_name
+        self._creature_name = creature_name
 
     # def __init__(self, creature_name, creature_type):
     #     self.__creature_name = creature_name
@@ -16,50 +15,44 @@ class Creature:
 
     def print_creature(self):
         print "hello"
-
+        # not implemented
+    
     def get_creature_name(self):
-        return self.__creature_name
+        return self._creature_name
 
 
 class Dragon(Creature):
-    __color = ""
-    __temp_of_fire = 0
 
     def __init__(self, color, temp_of_fire):
-        self.__color = color
-        self.__temp_of_fire = temp_of_fire
+        self._color = color
+        self._temp_of_fire = temp_of_fire
 
     def print_creature(self):
         print "This is a Dragon, is it %s and its' fire tempature is %d. Its' name is %s"%\
-              (self.__color, self.__temp_of_fire, self.get_creature_name())
+              (self._color, self._temp_of_fire, self.get_creature_name())
 
 
 class Giraffe(Creature):
-    __height = 0.0
-    __num_of_dots = 0
 
     def __init__(self, height, num_of_dots):
-        self.__height = height
-        self.__num_of_dots = num_of_dots
+        self._height = height
+        self._num_of_dots = num_of_dots
 
     def print_creature(self):
         print "This is a Giraffe, is it %d meters tall and has %d dots. Its' name is %s"%\
-              (self.__height, self.__num_of_dots, self.get_creature_name())
+              (self._height, self._num_of_dots, self.get_creature_name())
 
 
 class Monster(Creature):
-    __num_of_eyes = 0
-    __num_of_teeth = 0
-    __num_of_legs = 0
 
     def __init__(self, num_of_eyes, num_of_teeth, num_of_legs):
-        self.__num_of_eyes = num_of_eyes
-        self.__num_of_teeth = num_of_teeth
-        self.__num_of_legs = num_of_legs
+        self._num_of_eyes = num_of_eyes
+        self._num_of_teeth = num_of_teeth
+        self._num_of_legs = num_of_legs
 
     def print_creature(self):
         print "This is a Monster, is has %d eyes, %d teeth and %d legs. Its' name is %s"%\
-              (self.__num_of_eyes, self.__num_of_teeth, self.__num_of_legs,  self.get_creature_name())
+              (self._num_of_eyes, self._num_of_teeth, self._num_of_legs,  self.get_creature_name())
 
 
 def creature_factory(creature_name, features):
